12feeb542cb700da7580daaba3e4b8d882306212
zhanghongbo <zhanghb@mmwipo.com>
2017-03-03 16:27:21 +0800

    create project
    first

---
+++ MyRhino/MyRhino/Base/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  MyRhino
+//
+//  Created by Rhino on 2017/3/2.
+//  Copyright © 2017年 Rhino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
